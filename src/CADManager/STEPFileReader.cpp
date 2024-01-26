c915c107ea010a487d9cda8c075f6c618857ec03
pgilewicz <pawel.gilewicz1@gmail.com>
2024-01-26 19:32:25 +0100

    Added class for loading STEP files. Implemented method for reading step and assigning unique names if parts are not named originally.

---
+++ src/CADManager/STEPFileReader.cpp
@@ -0,0 +1,123 @@
+#include <filesystem>
+
+#include <vtkLogger.h>
+
+#include <XCAFApp_Application.hxx>
+
+#include "STEPFileReader.h"
+#include <TDF_Attribute.hxx>
+#include <TDataStd_Name.hxx>
+#include <TopExp_Explorer.hxx>
+#include <TopoDS.hxx>
+#include <TopoDS_Solid.hxx>
+#include <XCAFDoc_DocumentTool.hxx>
+#include <XCAFDoc_ShapeTool.hxx>
+
+std::string Importing::STEPFileReader::load(const std::string& fileName) {
+	if (!std::filesystem::exists(fileName)) {
+		auto message = "File " + fileName + " can not be found.";
+		vtkLogF(ERROR, message.c_str());
+		auto errorCode = std::make_error_code(std::errc::no_such_file_or_directory);
+		throw std::filesystem::filesystem_error(message, errorCode);
+	}
+	auto baseName = std::filesystem::path { fileName }.stem().generic_string();
+	vtkLogF(INFO, "Geometry file path: \"%s\"", fileName.c_str());
+
+	auto doc = Handle(TDocStd_Document) {};
+	auto app = XCAFApp_Application::GetApplication();
+	app->NewDocument("MDTV-XCAF", doc);
+	STEPCAFControl_Reader cafReader {};
+
+	// Reading colors mode
+	cafReader.SetColorMode(true);
+	// Reading layers information mode
+	cafReader.SetLayerMode(true);
+	// Reading names from step file mode
+	cafReader.SetNameMode(true);
+
+	IFSelect_ReturnStatus result = cafReader.ReadFile(fileName.c_str());
+	if (result != IFSelect_RetDone) {
+		auto message = "Error while reading file:" + fileName;
+		vtkLogF(ERROR, message.c_str());
+		auto errorCode = std::make_error_code(std::errc::device_or_resource_busy);
+		throw std::filesystem::filesystem_error(message, errorCode);
+	}
+
+	if (!cafReader.Transfer(doc)) {
+		auto message = "Error while reading file:" + fileName;
+		vtkLogF(ERROR, message.c_str());
+		auto errorCode = std::make_error_code(std::errc::device_or_resource_busy);
+		throw std::filesystem::filesystem_error(message, errorCode);
+	}
+
+	vtkLogF(INFO, "STEPCAFControl_Reader transferred successfully.");
+
+	auto& reader = cafReader.Reader();
+	auto partsMap = Importing::PartsMap {};
+	auto shapeTool = XCAFDoc_DocumentTool::ShapeTool(doc->Main());
+
+	auto numberOfShapes = reader.NbShapes();
+	if (numberOfShapes == 0) {
+		auto message = "No shapes found in given STEP file.";
+		vtkLogF(ERROR, message);
+		auto errorCode = std::make_error_code(std::errc::device_or_resource_busy);
+		throw std::filesystem::filesystem_error(message, errorCode);
+	} else {
+		for (auto i = 1; i <= numberOfShapes; i++) {
+			auto _shape = reader.Shape(i);
+
+			// Load each solid as an own object
+			auto explorer = TopExp_Explorer {};
+			for (explorer.Init(_shape, TopAbs_SOLID); explorer.More(); explorer.Next()) {
+
+				// Get the shape
+				auto& solid = TopoDS::Solid(explorer.Current());
+
+				auto uniqueName = getUniqueObjectName("Part", partsMap);
+				std::stringstream stringStream;
+				auto label = shapeTool->FindShape(solid);
+
+				if (!label.IsNull()) {
+					Handle(TDF_Attribute) attribute;
+					if (label.FindAttribute(TDataStd_Name::GetID(), attribute)) {
+						stringStream << Handle(TDataStd_Name)::DownCast(attribute)->Get();
+					}
+				}
+
+				auto message = "STEP: Transferring Shape " + stringStream.str();
+				vtkLogF(INFO, message.c_str());
+				if (stringStream.str().size() == 0) {
+					stringStream << "Solid";
+				}
+				partsMap[uniqueName] = solid;
+			}
+		}
+	}
+
+	return baseName;
+}
+
+std::string getUniqueObjectName(std::string prefix, const Importing::PartsMap& partsMap) {
+	// find already existing path that match prefix
+	std::vector<std::string> allNames;
+	for (const auto& partsMapIt : partsMap) {
+		// String object is the first element of parts map
+		const std::string stringObj = partsMapIt.first;
+		if (stringObj.find(prefix) != std::string::npos) {
+			allNames.push_back(stringObj);
+		}
+	}
+
+	int i = 0;
+	std::string uniqueName;
+	while (true) {
+		std::stringstream stringStream;
+		stringStream << prefix << std::setfill('0') << std::setw(3) << i;
+		uniqueName = stringStream.str();
+		auto res = std::find(std::begin(allNames), std::end(allNames), uniqueName);
+		if (res == std::end(allNames)) {
+			break;
+		}
+		i++;
+	}
+}
--- src/UserInterface/Rendering/QVTKRenderWindow.cpp
+++ src/UserInterface/Rendering/QVTKRenderWindow.cpp
@@ -1,7 +1,5 @@
 #include "QVTKRenderWindow.h"
 
-#include <QLayout>
-
 #include <vtkActor.h>
 #include <vtkAxesActor.h>
 #include <vtkCamera.h>
@@ -13,63 +11,55 @@
 #include <vtkProperty.h>
 #include <vtkTextProperty.h>
 
-namespace Rendering
-{
+#include <QLayout>
 
-QVTKRenderWindow::QVTKRenderWindow(QWidget* widget)
-    : _widget(widget)
-{
-    _vtkWidget = new QVTKOpenGLNativeWidget();
-    _renderer = vtkSmartPointer<vtkRenderer>::New();
-    _vtkWidget->setRenderWindow(vtkGenericOpenGLRenderWindow::New());
-    _vtkWidget->renderWindow()->AddRenderer(_renderer);
-    _vtkWidget->setFocusPolicy(Qt::StrongFocus);
-    generateCoordinateSystemAxes();
+Rendering::QVTKRenderWindow::QVTKRenderWindow(QWidget* widget)
+	: _widget(widget) {
+	_vtkWidget = new QVTKOpenGLNativeWidget();
+	_renderer = vtkSmartPointer<vtkRenderer>::New();
+	_vtkWidget->setRenderWindow(vtkGenericOpenGLRenderWindow::New());
+	_vtkWidget->renderWindow()->AddRenderer(_renderer);
+	_vtkWidget->setFocusPolicy(Qt::StrongFocus);
+	generateCoordinateSystemAxes();
 
-    // Background color
-    vtkNew<vtkNamedColors> colors;
-    _renderer->SetBackground(colors->GetColor3d("SlateGray").GetData());
+	// Background color
+	vtkNew<vtkNamedColors> colors;
+	_renderer->SetBackground(colors->GetColor3d("SlateGray").GetData());
 
-    _renderer->ResetCamera();
-    _widget->layout()->addWidget(_vtkWidget);
+	_renderer->ResetCamera();
+	_widget->layout()->addWidget(_vtkWidget);
 }
 
-QVTKRenderWindow::~QVTKRenderWindow() { }
+Rendering::QVTKRenderWindow::~QVTKRenderWindow() { }
 
-void QVTKRenderWindow::generateCoordinateSystemAxes()
-{
-    vtkSmartPointer<vtkAxesActor> axes = vtkSmartPointer<vtkAxesActor>::New();
-    _vtkAxesWidget = vtkSmartPointer<vtkOrientationMarkerWidget>::New();
+void Rendering::QVTKRenderWindow::generateCoordinateSystemAxes() {
+	vtkSmartPointer<vtkAxesActor> axes = vtkSmartPointer<vtkAxesActor>::New();
+	_vtkAxesWidget = vtkSmartPointer<vtkOrientationMarkerWidget>::New();
 
-    vtkSmartPointer<vtkProperty> shaftProps[] = {
-        axes->GetXAxisShaftProperty(), axes->GetYAxisShaftProperty(),
-        axes->GetZAxisShaftProperty()
-    };
+	vtkSmartPointer<vtkProperty> shaftProps[] = { axes->GetXAxisShaftProperty(),
+		axes->GetYAxisShaftProperty(),
+		axes->GetZAxisShaftProperty() };
 
-    vtkSmartPointer<vtkProperty> tipProps[] = {
-        axes->GetXAxisTipProperty(), axes->GetYAxisTipProperty(),
-        axes->GetZAxisTipProperty()
-    };
+	vtkSmartPointer<vtkProperty> tipProps[] = { axes->GetXAxisTipProperty(),
+		axes->GetYAxisTipProperty(),
+		axes->GetZAxisTipProperty() };
 
-    vtkSmartPointer<vtkTextProperty> labelProps[] = {
-        axes->GetXAxisCaptionActor2D()->GetCaptionTextProperty(),
-        axes->GetYAxisCaptionActor2D()->GetCaptionTextProperty(),
-        axes->GetZAxisCaptionActor2D()->GetCaptionTextProperty()
-    };
+	vtkSmartPointer<vtkTextProperty> labelProps[] = {
+		axes->GetXAxisCaptionActor2D()->GetCaptionTextProperty(),
+		axes->GetYAxisCaptionActor2D()->GetCaptionTextProperty(),
+		axes->GetZAxisCaptionActor2D()->GetCaptionTextProperty()
+	};
 
-    for (int i = 0; i < 3; ++i)
-    {
-        shaftProps[i]->SetColor(1.0, 1.0, 1.0);
-        tipProps[i]->SetColor(1.0, 1.0, 1.0);
-        labelProps[i]->ShadowOff();
-        labelProps[i]->ItalicOff();
-    }
+	for (int i = 0; i < 3; ++i) {
+		shaftProps[i]->SetColor(1.0, 1.0, 1.0);
+		tipProps[i]->SetColor(1.0, 1.0, 1.0);
+		labelProps[i]->ShadowOff();
+		labelProps[i]->ItalicOff();
+	}
 
-    _vtkAxesWidget->SetOrientationMarker(axes);
-    _vtkAxesWidget->SetInteractor(_vtkWidget->renderWindow()->GetInteractor());
-    _vtkAxesWidget->SetViewport(0.0, 0.0, 0.15, 0.25);
-    _vtkAxesWidget->SetEnabled(1);
-    _vtkAxesWidget->InteractiveOn();
+	_vtkAxesWidget->SetOrientationMarker(axes);
+	_vtkAxesWidget->SetInteractor(_vtkWidget->renderWindow()->GetInteractor());
+	_vtkAxesWidget->SetViewport(0.0, 0.0, 0.15, 0.25);
+	_vtkAxesWidget->SetEnabled(1);
+	_vtkAxesWidget->InteractiveOn();
 };
-
-} // namespace Rendering
