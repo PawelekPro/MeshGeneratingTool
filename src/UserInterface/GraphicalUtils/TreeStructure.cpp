2c1d2e878a19251a57e1a91d2e7bd054bca17593
pgilewicz <pawel.gilewicz1@gmail.com>
2024-02-11 21:14:28 +0100

    Created TreeStructure class. treewidget object of MainWindow promoted to TreeStructure type.

---
+++ src/UserInterface/GraphicalUtils/TreeStructure.cpp
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2024 Paweł Gilewicz
+ *
+ * This file is part of the Mesh Generating Tool. (https://github.com/PawelekPro/MeshGeneratingTool)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "TreeStructure.h"
+
+TreeStructure::TreeStructure(QWidget* parent)
+	: QTreeWidget(parent) {
+
+	QStringList headers; // Create list of headers
+	headers << tr("Label") << tr("Attribute") << tr("Visibility");
+	setHeaderLabels(headers); // Set headers for columns
+}
+
+TreeStructure::~TreeStructure() { }
