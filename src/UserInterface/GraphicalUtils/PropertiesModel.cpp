11460bc4e59a330196aceb278d9d4633bf6c1d33
pgilewicz <pawel.gilewicz1@gmail.com>
2024-03-19 19:44:03 +0100

    Created PropertiesModel and ModelFilter classes

---
+++ src/UserInterface/GraphicalUtils/PropertiesModel.cpp
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2024 Paweł Gilewicz
+ *
+ * This file is part of the Mesh Generating Tool. (https://github.com/PawelekPro/MeshGeneratingTool)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "PropertiesModel.h"
+
+//--------------------------------------------------------------------------------------
+bool ModelFilter::filterAcceptsRow(int sourceRow, const QModelIndex& sourceParent) const {
+	Q_UNUSED(sourceParent);
+
+	QAbstractItemModel* model = this->sourceModel();
+	model->data(model->index(sourceRow, 0), Qt::DisplayRole);
+
+	QModelIndex propIndex = model->index(sourceRow, 0);
+	QVariant propVariant = model->data(propIndex, Qt::UserRole);
+	QVariantMap propMap = propVariant.toMap();
+	bool hidden = propMap.value("hidden").toBool();
+
+	return !hidden;
+}
+
+//--------------------------------------------------------------------------------------
+PropertiesModel::PropertiesModel(QDomElement* element, QObject* parent)
+	: QAbstractTableModel(parent)
+	, _element(element) {
+
+	this->_header << "Property"
+				  << "Value";
+
+	QDomNodeList props = this->_element->childNodes();
+	for (int i = 0; i < props.length(); ++i) {
+		if (!props.at(i).isComment()) {
+			this->_properties.append(props.at(i).toElement());
+		}
+	}
+}
+
+//--------------------------------------------------------------------------------------
+PropertiesModel::~PropertiesModel() { }
+
+//--------------------------------------------------------------------------------------
+int PropertiesModel::rowCount(const QModelIndex& parent) const {
+	if (parent.isValid()) {
+		return this->_properties.count();
+	}
+	return 0;
+}
+
+//--------------------------------------------------------------------------------------
+int PropertiesModel::columnCount(const QModelIndex& parent) const {
+	if (parent.isValid()) {
+		return this->_header.size();
+	}
+	return 0;
+}
+
+//--------------------------------------------------------------------------------------
+QVariant PropertiesModel::data(const QModelIndex& index, int role) const {
+	if (!index.isValid())
+		return QVariant();
+
+	if (role != Qt::DisplayRole && role != Qt::EditRole)
+		return QVariant();
+
+	QDomElement propNode = this->_properties.value(index.row());
+	QVariant ret;
+	if (index.column() == 0) {
+		QDomAttr attr = propNode.attributes().namedItem("label").toAttr();
+		ret = attr.value();
+	} else if (index.column() == 1) {
+		ret = propNode.firstChild().toText().data();
+	}
+
+	return ret;
+}
---
+++ src/UserInterface/GraphicalUtils/PropertiesModel.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2024 Paweł Gilewicz
+ *
+ * This file is part of the Mesh Generating Tool. (https://github.com/PawelekPro/MeshGeneratingTool)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PROPERTIESMODEL_H
+#define PROPERTIESMODEL_H
+
+#include <QAbstractItemModel>
+#include <QAbstractTableModel>
+#include <QDomElement>
+#include <QModelIndex>
+#include <QSortFilterProxyModel>
+#include <QString>
+#include <QStringList>
+#include <Qt>
+
+class ModelFilter : public QSortFilterProxyModel {
+protected:
+	ModelFilter() = default;
+	~ModelFilter() = default;
+
+	bool filterAcceptsRow(int, const QModelIndex&) const override;
+};
+
+class PropertiesModel : public QAbstractTableModel {
+public:
+	PropertiesModel(QDomElement* element, QObject* parent = nullptr);
+	~PropertiesModel();
+
+	int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+
+	int columnCount(const QModelIndex& parent = QModelIndex()) const override;
+
+	QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+
+private:
+	QDomElement* _element;
+	QList<QDomElement> _properties;
+	QStringList _header;
+};
+
+#endif
