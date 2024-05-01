8972e5e357e243b9aba79295049f730c7d903fde
pgilewicz <pawel.gilewicz1@gmail.com>
2024-05-01 12:12:55 +0200

    Updated docstrings in AppDefaults class

--- src/UserInterface/Defaults/AppDefaults.h
+++ src/UserInterface/Defaults/AppDefaults.h
@@ -34,13 +34,32 @@
 namespace fs = std::filesystem;
 
 /**
- * @brief  Class for handling default application properties like name, version, etc.
+ * @brief The AppDefaults class provides access to application-wide default settings.
  *
+ * This class is designed to hold default settings and configurations that are used
+ * throughout the application. It follows the Singleton design pattern to ensure that
+ * only one instance of the class is created and provides a global point of access
+ * to that instance through the static method getInstance().
  */
 class AppDefaults {
 public:
 	AppDefaults();
 	~AppDefaults() = default;
+
+	/**
+	 * @brief Provides a global point of access to the singleton instance of the AppDefaults class.
+	 *
+	 * This static member function follows the Singleton design pattern to ensure that only one instance
+	 * of the AppDefaults class is created throughout the application's lifetime. It returns a reference
+	 * to the singleton instance. If the instance does not exist yet, it creates one. Subsequent calls
+	 * to this function return the same instance.
+	 *
+	 * @return A reference to the singleton instance of the AppDefaults class.
+	 */
+	static AppDefaults& getInstance() {
+		static AppDefaults instance;
+		return instance;
+	};
 
 	/**
 	 * @brief  Get name of application as a QString.
@@ -63,15 +82,24 @@ public:
 	 */
 	QString getAppProjFileVersion();
 
+	/**
+	 * @brief Returns the path to the templates directory.
+	 *
+	 * The templates directory contains predefined templates used by the application.
+	 *
+	 * @return The path to the templates directory as a QString.
+	 */
 	const QString getTemplatesPath();
 
-	const QString getComboBoxModelsPath();
-
-	static AppDefaults& getInstance() {
-		static AppDefaults instance;
-		return instance;
-	};
-
+	/**
+	 * @brief Returns a pointer to the ComboBox models stored in the AppDefaults class.
+	 *
+	 * This function provides access to the ComboBox models stored within the AppDefaults class.
+	 * It returns a pointer to a rapidjson::Document object containing the ComboBox models.
+	 * This allows external code to access and manipulate the models as needed.
+	 *
+	 * @return A pointer to the rapidjson::Document object containing the ComboBox models.
+	 */
 	rapidjson::Document* getComboBoxModels();
 
 private:
@@ -86,7 +114,28 @@ private:
 	 */
 	QString getValue(const QString& key);
 
+	/**
+	 * @brief Reads and parses a JSON file.
+	 *
+	 * This function reads the content of a JSON file specified by the filePath parameter
+	 * and parses it into the provided rapidjson::Document object, doc. It opens the file,
+	 * reads its content, closes the file, and then parses the JSON data into the document.
+	 * If any errors occur during the process, it logs an error message.
+	 *
+	 * @param doc A reference to a rapidjson::Document object where the parsed JSON data will be stored.
+	 * @param filePath The path to the JSON file to be read and parsed.
+	 */
 	void readJSONFile(rapidjson::Document&, QString);
+
+	/**
+	 * @brief Returns the path to the ComboBox models directory.
+	 *
+	 * The ComboBox models directory contains predefined models used for ComboBoxes
+	 * within the application.
+	 *
+	 * @return The path to the ComboBox models directory as a QString.
+	 */
+	const QString getComboBoxModelsPath();
 
 	// Container for application settings
 	QSettings settings;
