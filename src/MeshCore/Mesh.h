f1ee298d4aa073a008be6d9cea63f04d2d2d51e7
KFudali <krystian.fudali42@gmail.com>
2024-05-23 19:39:52 +0200

    started movig mesh logic to mesh class

--- src/MeshCore/Mesh.h
+++ src/MeshCore/Mesh.h
@@ -1,11 +1,59 @@
 #ifndef MESH_H
 #define MESH_H
 
+#ifdef _WIN32
+#include <gmsh.h_cwrap>
+#endif
+
+#ifdef linux
+#include <gmsh.h>
+#endif
+
+#include <algorithm>
+#include <vector>
+
+#include <vtkActor.h>
+#include <vtkCellArray.h>
+#include <vtkPoints.h>
+#include <vtkPolyData.h>
+#include <vtkSmartPointer.h>
+#include <vtkUnstructuredGrid.h>
+#include <vtkDataSetMapper.h>
+#include <vtkIdTypeArray.h>
+
+
 namespace MeshCore{
 
     class Mesh{
         public:
+
+            Mesh(){};
+            ~Mesh(){};
+
+            vtkSmartPointer<vtkActor> getMeshActor(){return this->_meshActor;};
+
+            void addSizing(std::vector<int> nodeToSizeTags);
+            void genereateSurfaceMesh();
+            void generateVolumeMesh();
+
         private:
+            
+            void updateMeshActor();
+
+            enum class ElementType : int {
+                TETRA = 4,
+                TRIANGLE = 2,
+                LINE = 1,
+                POINT = 15,
+            };
+
+            vtkSmartPointer<vtkActor> _meshActor;
+
+            std::vector<std::pair<int, int>> _sizingTags;
+            
+            vtkSmartPointer<vtkUnstructuredGrid> _gridData;
+            vtkSmartPointer<vtkPolyData> _polyData;
+
     };
 };
 
