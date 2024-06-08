41efd0ff1776d1fe3df7d38313a06e4e36404aed
KFudali <krystian.fudali42@gmail.com>
2024-06-08 12:21:25 +0200

    correct document handler template

--- src/UserInterface/Core/DocumentHandler.h
+++ src/UserInterface/Core/DocumentHandler.h
@@ -1,13 +1,43 @@
 #ifndef DOCUMENTHANDLER_H
 #define DOCUMENTHANDLER_H
 
-#include "TreeStructure.h"
+
+#include "AppDefaults.h"
+#include "document.h"
+#include "istreamwrapper.h"
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include <QDebug>
+#include <QFile>
+#include <QHeaderView>
+#include <QMap>
+#include <QPointer>
+#include <QSharedPointer>
+#include <QStringList>
+#include <QTextStream>
+#include <QTreeWidget>
+#include <QTreeWidgetItem>
+#include <QtXml/QDomDocument>
+#include <QtXml/QDomElement>
 
 
-class doc : public TreeStructure{
-    int a;
-}
+class DocumentHandler{
+    
+    public:
 
+
+
+
+
+    private:
+        QDomDocument _document;
+        
+
+};
 
 
 
