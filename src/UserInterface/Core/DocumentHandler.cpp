395273e0c9e4bb2315ebdfb3fce39d0916551679
KFudali <krystian.fudali42@gmail.com>
2024-06-08 11:23:54 +0200

    moved the document handler to Core folder

---
+++ src/UserInterface/Core/DocumentHandler.cpp
@@ -0,0 +1,1 @@
+#include "DocumentHandler.cpp"
---
+++ src/UserInterface/Core/DocumentHandler.h
@@ -0,0 +1,14 @@
+#ifndef DOCUMENTHANDLER_H
+#define DOCUMENTHANDLER_H
+
+#include "TreeStructure.h"
+
+
+class doc : public TreeStructure{
+    int a;
+}
+
+
+
+
+#endif
